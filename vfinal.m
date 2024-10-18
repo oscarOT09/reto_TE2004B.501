71c4398e6fe092c65de0218ce8dda7e77f8f85e8
oscar_ot09 <135353255+oscarOT09@users.noreply.github.com>
2024-10-18 14:45:06 -0600

    Versión presentación | Etapa 1

--- vfinal.m
+++ vfinal.m
@@ -1,3 +1,12 @@
+% Implementación del algoritmo de estimación de la velocidad de un vehículo
+
+% Paper de referencia:
+% "Speed Estimation On Moving Vehicle Based On Digital Image Processing"
+% (https://www.researchgate.net/publication/317312246)
+
+% Autores: Oscar Ortiz Torres, Yonathan Romero Amador y Emmanuel Lechuga
+% Arreola
+
 clc
 clear all
 close all
@@ -8,99 +17,94 @@ H = 1.7; % Altura de la cámara en m
 v = 0.012; %% Dimensión vertical del formato de imagen
 f = 0.003; % Distancia focal de la cámara (8-25)
 
-Tc = 2*atan(v/(2*f));
-T = Tv + Tc/2;
-D = H*tan(T);
-P = 2*tan(Tc/2)*sqrt(H^2+D^2);
-I_height = 1080; % Alto en pixeles de la imagen
-k = P / I_height;
-
 foregroundDetector = vision.ForegroundDetector('NumGaussians', 5, ...
                                                'NumTrainingFrames', 40, ...
                                                'LearningRate', 0.005, ...
                                                'MinimumBackgroundRatio', 0.7);
-pre = [0 0];
+prev_centroide = [0 0];
 vel_vector=[];
 
 %% A. Video Input
-videoSource  = VideoReader("oscar_20_1.mp4");
-fps = videoSource.FrameRate;
+videoFuente  = VideoReader("oscar_20_1.mp4");
 
-while hasFrame(videoSource)
+fps = videoFuente.FrameRate;
+
+Tc = 2*atan(v/(2*f)); % Campo de visión de la cámara
+T = Tv + Tc/2; % Ángulo total de visión (suma del ángulo de la cámara y el ángulo de proyección)
+D = H*tan(T); % Distancia de la cámara al objeto
+P = 2*tan(Tc/2)*sqrt(H^2+D^2); % Proyección del campo de visión (en metros)
+I_height = videoFuente.Height; % Altura del video en pixeles
+k = P / I_height; % Factor de conversión de pixeles a metros
+
+while hasFrame(videoFuente)
     %% B. Preprocessing
-    img  = readFrame(videoSource); 
-    img_pre = im2gray(img); % Convertir a escala de grises
-    img_pre = imadjust(img_pre, [0.2 0.7], [0 1], 1.8); %Si
-    %img_pre = medfilt2(img_pre, [3 3]);
+    % Lectura de frame, se convierte de RGB a escala de grises y se ajusta
+    % su contraste
+    img  = readFrame(videoFuente);
+    img_pre = im2gray(img);
+    img_pre = imadjust(img_pre, [0.2 0.7], [0 1], 1.8);
     
-    %% C. Background Subs (restar el fondo del frame actual)
+    %% C. Background Subs (restar el fondo del frame actual) & E. Shadow Removal
+    % Resta el fondo del frame actual usando el detector, dando una imagen
+    % binaria como salida
     bg_sub = step(foregroundDetector,img_pre);
 
     %% D. Smoothing
+    % Aplicación de filtro para reducir el ruido a la imagen
     img_suav = medfilt2(bg_sub,[4 4]);
 
-    %% E. Shadow removal
-    %img_suav(img_suav == 127) = 0;  % Marcar las sombras como fondo
-    %img_suav(img_suav == 255) = 1;  % Los objetos en movimiento se quedan como primer plano
-
     %% F. Operaciones morfológicas para eliminar ruido
+    % Realiza una apertura (para eliminar objetos pequeños)
+    % y un cierre (para rellenar huecos en los objetos)
     img_morfo = imopen(img_suav, strel('square', 15));
     img_morfo = imclose(img_morfo, strel('square', 30));
 
-    %
     %% G. Object detection
-    % Etiquetar las regiones conectadas
+    % Guardado de objetos y extracción de propiedades
     [labels, num] = bwlabel(img_morfo);
-
-    % Obtener propiedades de cada objeto detectado
     stats = regionprops(labels, 'BoundingBox', 'Centroid','Area');
-
-    imshow(img); % Mostrar el frame original
-    hold on;
     
     %% H. Labeling and tracking
-    % Dibujar los rectángulos y centroides en los objetos detectados
+    % Identificación y seguimiento de los objetos detectados
+    imshow(img); % Muestra el frame original
+    hold on;
+
     for i = 1:num
         if(stats(i).Area > 8000)
-            %fprintf('Area: %.2f\n', stats(i).Area)
-
-            % Extraer el rectángulo y el centroide
-            bbox = stats(i).BoundingBox; % Coordenadas del rectángulo [x, y, width, height]
-            centroide = stats(i).Centroid; % Coordenadas [x, y]
+            % Extracción de coordenadas de rectángulo y centroide
+            bbox = stats(i).BoundingBox;
+            centroide = stats(i).Centroid;
             
-            % Dibujar el rectángulo delimitador
+            % Dibujo de rectángulos y centroides en los objetos detectados
             rectangle('Position', bbox, 'EdgeColor', 'y', 'LineWidth', 2);
-            
-            % Dibujar el centroide
             plot(centroide(1), centroide(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
             
             %% I. Speed Estimation
-            d = sqrt(sum((pre-centroide).^2));
-            v = k*(d/(1/fps));
-            v = 3.6*v;
-
-            pre(1) = centroide(1);
-            pre(2) = centroide(2);
+            % Cálculo de la distancia Euclidiana respecto al frame anterior
+            d_e = sqrt(sum((prev_centroide-centroide).^2));
+            
+            % Cálculo de la velocidad en km/h
+            v = 3.6*(k*(d_e/(1/fps)));
             vel_vector = [vel_vector,v];
 
-            % Obtener la posición del texto (encima del rectángulo)
-            pos_x = bbox(1); % Coordenada X del rectángulo
-            pos_y = bbox(2) - 10; % Coordenada Y ligeramente encima del rectángulo
+            % Guardado de coordenadas para la siguiente iteración
+            prev_centroide(1) = centroide(1);
+            prev_centroide(2) = centroide(2);
 
-            % Mostrar la velocidad encima del objeto con fondo
+            % Impresión de la velocidad encima del objeto
             texto_vel = sprintf('Vel: %.2f km/h', v);
-            text(pos_x, pos_y, texto_vel, 'Color', 'yellow', 'FontSize', 12, 'FontWeight', 'bold', ...
-                 'BackgroundColor', 'black', 'EdgeColor', 'yellow', 'Margin', 3);
+            text(bbox(1), bbox(2)-10, texto_vel, 'Color', 'blue', 'FontSize', 12, 'FontWeight', 'bold', ...
+                 'BackgroundColor', 'yellow');
         end
     end
     
     hold off;
-    drawnow; % Actualizar el gráfico en cada iteración
+    drawnow; % Actualización el gráfico en cada iteración
 end
 
-%figure
-%plot(centroides_x)
-%figure
-vel_vector(1) = [];
-%stem(vel_vector)
-fprintf('Velocidad del objeto: %.2f\n', mean(vel_vector))
+vel_vector(1) = []; % Eliminación de la primera muestra
+
+% Impresión de la velocidad promedio del objeto
+texto_velf = sprintf('Velocidad del objeto: %.2f km/h', mean(vel_vector));
+text(10, 30, texto_velf, 'Color', 'blue', 'FontSize', 12, 'FontWeight', 'bold', ...
+                 'BackgroundColor', 'yellow');
